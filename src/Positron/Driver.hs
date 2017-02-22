f6da637d200878089d0211bd83f9d1b6381added
XT <e@xtendo.org>
2017-02-22 15:54:48 +0900

    Bump Stackage nightly

--- src/Positron/Driver.hs
+++ src/Positron/Driver.hs
@@ -10,7 +10,7 @@ import Positron.Import
 
 -- data types
 
-import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
